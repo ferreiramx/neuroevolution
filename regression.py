53ea88c46dc3847b80c11c54fb4b11980f754f61
Angel Ferreira <angel.fgsfds@gmail.com>
2018-10-16 10:13:09 -0500

    Add hidden layers

--- regression.py
+++ regression.py
@@ -12,7 +12,6 @@ global_best = -99999999
 dataset = pd.read_csv(sys.argv[1], thousands=',')
 rows = dataset.shape[0]
 cols = dataset.shape[1]
-print("Rows: ", rows, "Columns: ", cols)
 vars = dataset[dataset.columns[0:(cols-2)]]
 target = dataset[dataset.columns[(cols-1)]]
 rrse_list = [-9999999] * rows
@@ -22,7 +21,7 @@ generations = 0
 #################################################
 
 params = NEAT.Parameters()
-params.PopulationSize = 50
+params.PopulationSize = 100
 params.DynamicCompatibility = True
 params.NormalizeGenomeSize = True
 params.WeightDiffCoeff = 0
@@ -31,7 +30,7 @@ params.YoungAgeTreshold = 15
 params.SpeciesMaxStagnation = 15
 params.OldAgeTreshold = 35
 params.MinSpecies = 1
-params.MaxSpecies = 250
+params.MaxSpecies = 75
 params.RouletteWheelSelection = False
 params.RecurrentProb = 0.1
 params.OverallMutationRate = 0.3
@@ -48,7 +47,7 @@ params.WeightReplacementRate = 0.9
 
 params.MaxWeight = 50000.0
 
-params.MutateAddNeuronProb = 0.5
+params.MutateAddNeuronProb = 0.6
 params.MutateAddLinkProb = 0.3
 params.MutateRemLinkProb = 0.1
 params.MutateRemSimpleNeuronProb = 0.1
@@ -62,14 +61,14 @@ params.ActivationFunction_Tanh_Prob = 0
 params.ActivationFunction_SignedStep_Prob = 0
 params.ActivationFunction_Linear_Prob = 0.33
 params.ActivationFunction_Relu_Prob = 0.33
-params.ActivationFunction_Softmax_Prob = 0.33
+params.ActivationFunction_Softmax_Prob = 0
 
-params.CrossoverRate = 0.6
-params.MultipointCrossoverRate = 0.6
+params.CrossoverRate = 0.7
+params.MultipointCrossoverRate = 0.4
 params.SurvivalRate = 0.3
 
-params.MutateNeuronTraitsProb = 0.3
-params.MutateLinkTraitsProb = 0.3
+params.MutateNeuronTraitsProb = 0.4
+params.MutateLinkTraitsProb = 0.4
 
 params.AllowLoops = True
 params.AllowClones = True
@@ -126,10 +125,10 @@ def evaluate(genome, skip_idx):
         # print("Network output: ", output[0])
         testrrse = np.sqrt(((target.iat[skip_idx] - output[0]) ** 2) / ((target.iat[skip_idx] - avgOutput) ** 2)) * 100
         testmae = abs(target.iat[skip_idx] - output[0])
-        #if generations > 0:
-            #print("[GEN ", generations, "] New Best Training Error (RRSE):", -rrse)
+        if generations > 0:
+            print("[ROW ", skip_idx, "][GEN ", generations, "] New Best Training Error (RRSE):", -rrse)
             #print("Training Error (MAE): ", -mae)
-            #print("Test Error (RRSE): ", testrrse)
+            print("Test Error (RRSE): ", testrrse)
             #print("Test Error (MAE): ", testmae)
         rrse_list[skip_idx] = testrrse
         mae_list[skip_idx] = testmae
@@ -147,20 +146,23 @@ def evolve():
     global cols
 
     # print("LOO Validation:")
-    g = NEAT.Genome(0, cols, 0, 1, False, NEAT.ActivationFunction.LINEAR, NEAT.ActivationFunction.LINEAR, 0, params, 0)
+    g = NEAT.Genome(0, cols, (cols-1), 1, False, NEAT.ActivationFunction.RELU, NEAT.ActivationFunction.LINEAR, 1, params, 1)
     for test_idx in range(rows):
         pop = NEAT.Population(g, params, True, 1.0, 0)
         pop.RNG.Seed(int(time.clock() * 100))
         generations = 0
         global_best = -99999999
         no_improvement = 0
-        while generations < 10:
-            # if (generations > 20 and global_best < -100):
-            #     pop = NEAT.Population(g, params, True, 1.0, 0)
-            #     pop.RNG.Seed(int(time.clock() * 100))
-            #     generations = 0
-            #     global_best = -99999999
-            #     no_improvement = 0
+        # Run for a maximum of N generations
+        while generations < 50: #TODO: make max gens into variable and set via command line
+             # Reset the population if this path does not seem promising
+            if (generations > 10 and global_best < -200):
+                 pop = NEAT.Population(g, params, True, 1.0, 0)
+                 pop.RNG.Seed(int(time.clock() * 100))
+                 generations = 0
+                 global_best = -99999999
+                 no_improvement = 0
+
             genome_list = NEAT.GetGenomeList(pop)
             fitness_list = []
             for genome in genome_list:
@@ -169,17 +171,17 @@ def evolve():
             pop.Epoch()
             generations += 1
             best = max(fitness_list)
-            print("[", test_idx, "] ", -global_best, " (", no_improvement, ")")
+            #print("[ROW:", test_idx, "] ", -global_best, " (", no_improvement, " g. of no improvement)")
             if best > global_best:
                 no_improvement = 0
                 global_best = best
             else:
                 no_improvement += 1
 
-        # print("LOO test error (RRSE):")
-        # print(rrse_list[test_idx])
-        # print("LOO test error (MAE):")
-        # print(mae_list[test_idx])
+        #print("LOO test error (RRSE):")
+        #print(rrse_list[test_idx])
+        #print("LOO test error (MAE):")
+        #print(mae_list[test_idx])
 
     print(rrse_list)
     print(mae_list)
@@ -188,17 +190,17 @@ def evolve():
     return [avg_rrse, avg_mae]
 
 
-avg_score = 0
-avg_mae = 0
-nruns = 2
+nruns = 1   # TODO: Add support for declaring number of runs on the command line
+avg_rrse = [0.0] * nruns
+avg_mae = [0.0] * nruns
 for run in range(nruns):
     score = evolve()
-    avg_score += score[0]
-    avg_mae += score[1]
+    avg_rrse[run] = score[0]
+    avg_mae[run] = score[1]
     print("Run [", run, "] Test RRSE: ", score[0])
     print("Run [", run, "] Test MAE: ", score[1])
 
-avg_score /= nruns
-avg_mae /= nruns
-print("Average RRSE from all runs: ", avg_score)
-print("Average MAE from all runs: ", avg_mae)
+print("RRSE from every run: ", avg_rrse)
+print("MAE from every run: ", avg_mae)
+print("Average RRSE from all runs: ", np.mean(avg_rrse))
+print("Average MAE from all runs: ", np.mean(avg_mae))
